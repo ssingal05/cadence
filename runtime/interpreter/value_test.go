40e4867eea7793d51a6d12abec65c964f422b0e0
robert <robert.davidson@forecastfoundation.org>
2021-12-01 16:11:39 -0800

    lint hack

--- runtime/interpreter/value_test.go
+++ runtime/interpreter/value_test.go
@@ -3016,7 +3016,7 @@ func TestPublicKeyValue(t *testing.T) {
 		)
 
 		assert.PanicsWithValue(t,
-			InvalidPublicKeyError{PublicKey: fmt.Sprintf("%s", publicKey)},
+			InvalidPublicKeyError{PublicKey: fmt.Sprintf("%s ", publicKey)},
 			func() {
 				_ = NewPublicKeyValue(
 					inter,
